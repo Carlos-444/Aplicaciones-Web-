eaf9732ee29f9fcf7b4169c3ab92c08ceee03c04
Carlos-444 <juancarloscamargo770@gmail.com>
2026-09-17 17:18:01 -0600

    Trabajos como introduccion a php y la estructura de html5

---
+++ index.php
@@ -0,0 +1,44 @@
+ <!--    <?php
+    echo "Hola, Mundo!";
+    ?> -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+
+    <style>
+    h1{
+    
+    color: blue;
+    }
+
+    div {
+        background: #20e697;
+    }
+
+    
+
+    </style>
+</head>
+<body>
+    <h1>Hola Mundo</h1>
+    <h2>Hola Mundo</h2>
+    <h3>Hola Mundo</h3>
+    <h4>Hola Mundo</h4>
+    <h5>Hola Mundo</h5>
+    <h6>Hola Mundo</h6>
+
+     <div>
+        <h2>Titulo del parrafo</h2>
+    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque iste fugit, minus dicta dolorum facilis! Dolores pariatur, ut accusamus molestiae sint eius, minima non facilis ullam velit, voluptates nam in. </p>
+    </div>
+</body>
+</html>
+
+
+
+
---
+++ prueba.py
@@ -0,0 +1,10 @@
+
+
+nero = 0
+print ("Hola word") 
+
+if nero == 0:
+    print("vas bien.")
+else:
+    print("Este archivo ha sido importado desde otro módulo.")
+
